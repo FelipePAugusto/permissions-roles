74f1ba7bd7903da6072fe2d970c2ebec1f005227
Felipe Augusto <felipenax@gmail.com>
2024-01-23 18:02:42 -0300

    feat: criando accessor para password

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function password(): Attribute
+    {
+        return new Attribute(set: function($value) {
+            if(!empty($value) && !is_null($value)) {
+                return bcrypt($value);
+            }
+        });
+    }
+    
 }
