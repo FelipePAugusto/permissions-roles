a9883893f18c58ddbf4ac787790b96afc1257efa
Felipe Augusto <felipenax@gmail.com>
2024-02-01 11:42:07 -0300

    feat: aprimorando a tela de login

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
+    @vite(['resources/css/app.css'])
 </head>
-<body>
+<body class="h-full">
     @yield('content')
 </body>
 </html>
